754a24b665b6a0407392be125f4ee9a7d187b5ed
Jonathan Pilling <jpilling@progrexion.com>
2020-05-29 17:16:34 -0600

    working on tests, pageTitle getter doesn't seem to be working

--- lex-page-objects/src/main/java/lex/pageobjects/homepage/LexHomepage.java
+++ lex-page-objects/src/main/java/lex/pageobjects/homepage/LexHomepage.java
@@ -2,10 +2,33 @@ package lex.pageobjects.homepage;
 
 import org.openqa.selenium.WebDriver;
 import lex.framework.core.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 public class LexHomepage extends BasePage {
 
+    private static String URL = "https://www.lexingtonlaw.com";
+
+    /*Locators*/
+    @FindBy(how = How.CSS, using = "title")
+    private WebElement pageTitle;
+
+    @FindBy(how = How.CSS, using = "a[class='site_nav_sign_up_desktop']")
+    private WebElement navSignUpButton;
+
     public LexHomepage(WebDriver driver) {
         super(driver);
+        driver.get(URL);
     }
+
+    public WebElement getPageTitle() {
+        return pageTitle;
+    }
+
+    public void clickSignUp() {
+        navSignUpButton.click();
+    }
+
+
 }
--- lex-regression-tests/src/test/java/lex/tests/homepage/LexHomepageTests.java
+++ lex-regression-tests/src/test/java/lex/tests/homepage/LexHomepageTests.java
@@ -1,14 +1,25 @@
 package lex.tests.homepage;
 
 import org.testng.annotations.Test;
+import org.testng.Assert;
 import lex.framework.core.BaseTest;
 import lex.pageobjects.homepage.LexHomepage;
 
 public class LexHomepageTests extends BaseTest {
 
     @Test
-    public void homepageTests() {
-        LexHomepage googleHomepage = new LexHomepage(getDriver());
-    }55
+    public void IsAtHomepage() {
+        LexHomepage myPage = new LexHomepage(getDriver());
+        System.out.println("Page Title: " + myPage.getPageTitle().getText());
+        Assert.assertTrue(myPage.getPageTitle().getText() == "Lexington Law Firm " +
+                "| Trusted Attorneys Helping to Fix Your Credit");
+    }
+    @Test
+    public void canClickSignUp() {
+        LexHomepage myPage = new LexHomepage(getDriver());
+        myPage.clickSignUp();
+        System.out.println("Page Title: " + myPage.getPageTitle().getText());
+        Assert.assertTrue(myPage.getPageTitle().getText() == "Sign Up");
+    }
 }
 
