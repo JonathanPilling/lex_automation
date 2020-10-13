5f884c0ef24381094d378947116d5fe868fa82b8
Jonathan Pilling <184973@pgx180189mac.local>
2020-10-13 13:26:19 -0600

    got rid of no sandbox

--- lex-framework/src/main/java/lex/framework/core/BaseTest.java
+++ lex-framework/src/main/java/lex/framework/core/BaseTest.java
@@ -4,8 +4,6 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -25,15 +23,9 @@ public class BaseTest {
         ChromeDriverManager.chromedriver();
         if(headless) {
 
-            DesiredCapabilities crcapabilities = DesiredCapabilities.chrome();
-            crcapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-            crcapabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--headless");
-            chromeOptions.addArguments("--no-sandbox");
             chromeOptions.addArguments("--window-size=1920,1080");
-            crcapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
             driver = new ChromeDriver(chromeOptions);
         } else {
             driver = new ChromeDriver();
